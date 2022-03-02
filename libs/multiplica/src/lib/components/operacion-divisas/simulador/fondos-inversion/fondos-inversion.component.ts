195fe6c26c0cda95a46309fdd79f5931d37d35d8
Jorge A. Pérez <armando@multiplica.com>
2022-03-02 04:27:30 -0600

    Fondos de inversion simulador

--- libs/multiplica/src/lib/components/operacion-divisas/simulador/fondos-inversion/fondos-inversion.component.ts
+++ libs/multiplica/src/lib/components/operacion-divisas/simulador/fondos-inversion/fondos-inversion.component.ts
@@ -2,17 +2,95 @@ import { Component, OnInit } from '@angular/core';
 import { Document } from '../../../../models/documents.interface';
 import { DocumentsService } from 'libs/multiplica/src/lib/services/documents.service';
 
-
 @Component({
   selector: 'app-fondos-inversion',
   templateUrl: './fondos-inversion.component.html',
-  styleUrls: ['./fondos-inversion.component.scss']
+  styleUrls: ['./fondos-inversion.component.scss'],
 })
 export class FondosInversionComponent implements OnInit {
+  tables = [
+    { cabezera: 'Fondo' },
+    {
+      cabezera: 'Plazo máximo',
+    },
+    {
+      cabezera: 'En el año',
+    },
+    {
+      cabezera: 'En el mes',
+    },
+    {
+      cabezera: '7 días',
+    },
+    {
+      cabezera: '28 días',
+    },
+    {
+      cabezera: '12 meses',
+    },
+  ];
 
-  constructor() { }
+  data = [
+    {
+      fondo: 'Some fondo',
+      plazo: '1 año',
+      en_anio: '2022',
+      en_mes: '02',
+      dias_7: '500',
+      dias_28: '3500',
+      meses: '12',
+    },
+    {
+      fondo: 'Other Fondo',
+      plazo: '2 años',
+      en_anio: '2022',
+      en_mes: '02',
+      dias_7: '500',
+      dias_28: '3500',
+      meses: '12',
+    },
+    {
+      fondo: 'Next fondo',
+      plazo: '3 años',
+      en_anio: '2022',
+      en_mes: '02',
+      dias_7: '500',
+      dias_28: '3500',
+      meses: '12',
+    },
+  ];
 
-  ngOnInit(): void {
+  table_detalle = [
+    { cabezera: 'Fondo' },
+    {
+      cabezera: 'Tipo',
+    },
+    {
+      cabezera: '%',
+    },
+  ];
 
-  }
+  data_detalle = [
+    {
+      fondo: 'Some fondo',
+      tipo: 'some',
+      porcentaje: 'some',
+    },
+    {
+      fondo: 'Some fondo',
+      tipo: 'some',
+      porcentaje: 'some',
+    },
+    {
+      fondo: 'Some fondo',
+      tipo: 'some',
+      porcentaje: 'some',
+    },
+  ];
+
+  total = 0;
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
