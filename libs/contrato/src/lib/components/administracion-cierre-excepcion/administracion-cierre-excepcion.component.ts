be91d0db74e9c17f6f4ef0bcc35ed7e4c352593a
MTP\mtpqro070 <jair.barcenas@mtpinternational.mx>
2022-07-25 22:00:54 -0500

    se arrglan nuevos bug y reabiertos del 25/07/2022

--- libs/contrato/src/lib/components/administracion-cierre-excepcion/administracion-cierre-excepcion.component.ts
+++ libs/contrato/src/lib/components/administracion-cierre-excepcion/administracion-cierre-excepcion.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { AdministracionCierreExcepcionService } from '../../services/admin-cierre-excepcion.service';
 import { UsuarioVO, CierreExcepVO, ProfesionVO } from '@intercam/model';
@@ -6,6 +6,9 @@ import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms'
 import Swal from 'sweetalert2';
 import { SucursalVO } from 'libs/model/src/lib/com/intercam/corporativo/centrocostos/vo/SucursalVO';
 import { MatPaginator } from '@angular/material/paginator';
+import { ConsultaInfoGralEvent } from '../../util/ConsultaInfoGralEvent';
+import { MatSort, Sort } from '@angular/material/sort';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
 @Component({
   selector: 'intercam-administracion-cierre-excepcion',
   templateUrl: './administracion-cierre-excepcion.component.html',
@@ -15,8 +18,10 @@ import { MatPaginator } from '@angular/material/paginator';
 
 export class AdministracionCierreExcepcionComponent implements OnInit {
   @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
   displayedColumns: string[] = ['idCce', 'perId', 'claveOperador', 'nombre', 'claveSucursal', 'habilitadoCierre'];
   dataSource = new MatTableDataSource<UsuarioVO>();
+  dataSourceBkp: UsuarioVO[]=[];
   funcForm: FormGroup;
 
   perId: number; //identificador de la persona 
@@ -34,24 +39,37 @@ export class AdministracionCierreExcepcionComponent implements OnInit {
   mensajeError: string;
   submitted: boolean = false;
   paginador: boolean;
-  optSiDisabled:boolean;
-  optNoDisabled:boolean;
-  constructor(private ACEService: AdministracionCierreExcepcionService, private formBuilder: FormBuilder) {
+  optSiDisabled: boolean;
+  optNoDisabled: boolean;
+  sessionData: any;
+  constructor(private ACEService: AdministracionCierreExcepcionService, private formBuilder: FormBuilder, private _liveAnnouncer: LiveAnnouncer) {
     this.perId = Number(sessionStorage.getItem('perId'));
-
+    this.sessionData = JSON.parse(sessionStorage.getItem('usuarioVO'));
     console.log('perId: ', this.perId);
+    console.log('sessionData: ', this.sessionData);
     this.showBtnDes = true;
     this.showBtn = false;
 
   }
+
+
+  ngOnInit(): void {
+    this.clave = this.sessionData.usuClave;
+    console.log(this.clave);
+    this.createFunForm();
+    this.deshabilitarCampos();
+    this.consulta();
+    this.atributosElemento();
+  }
+
   onSubmit() {
     this.submitted = true;
     console.log("Guardando...");
-    this.funcForm.value;
+    this.funcForm.valid;
   }
   filtrar(e: any) {
-    console.log(e,e.key);
-    if(this.funcForm.get('busqueda').value == ''){
+    console.log(e, e.key);
+    if (this.funcForm.get('busqueda').value == '') {
       this.selectedAdmon = '';
       return this.dataSource.filter = '';
     }
@@ -92,13 +110,13 @@ export class AdministracionCierreExcepcionComponent implements OnInit {
     this.perId = userSelec.idPersona;
     this.idCce = userSelec.cierreExcep.idCce;
     this.funcForm.get('perNom').setValue(userSelec.perNomCorto);
-    if (userSelec.cierreExcep.cceCierreexcep == 'Si') {
+    if (userSelec.cierreExcep.cceCierreexcep == 'V') {
       this.checksi = true;
       this.funcForm.get('optSi').setValue(true);
       this.checkno = false;
       this.funcForm.get('optNo').setValue(false);
       this.cceCierreexcep = 'V';
-    } else if (userSelec.cierreExcep.cceCierreexcep == 'No') {
+    } else if (userSelec.cierreExcep.cceCierreexcep == 'F') {
       this.checkno = true;
       this.funcForm.get('optNo').setValue(true);
       this.checksi = false;
@@ -127,37 +145,44 @@ export class AdministracionCierreExcepcionComponent implements OnInit {
   }
   updateAce(el: any): void {
     this.submitted = true;
+    this.funcForm.valid;
     console.log(el);
     console.log(this.perId);
     console.log(this.clave);
     console.log(this.checksi);
     console.log(this.checkno);
     let cierreExcepVO = new CierreExcepVO;
-    console.log(cierreExcepVO);
+
     cierreExcepVO.idCce = this.idCce;
     cierreExcepVO.perId = Number(this.perId);
     cierreExcepVO.cceUsuabre = this.clave;
     cierreExcepVO.cceUsucierra = this.clave;
-    
+
     if (this.funcForm.get("optSi").value == true) {
       cierreExcepVO.cceCierreexcep = 'V';
-    } 
-    if(this.funcForm.get("optNo").value == true) {
+    }
+    if (this.funcForm.get("optNo").value == true) {
       cierreExcepVO.cceCierreexcep = 'F';
     }
-    if(this.funcForm.valid){
+    cierreExcepVO.cceFechaabre = null;
+    cierreExcepVO.cceFechacierra = null;
+    console.log(cierreExcepVO);
+
     this.ACEService.getActualizarOperador(cierreExcepVO).subscribe(
       then => {
+        console.log('Actualización Exitosa', then);
         this.mostrarMensaje('Actualización Exitosa', 'info');
         this.limpiarFormulario(el);
 
       },
       error => {
-        console.error(error);
+        console.log(error);
+        this.mostrarMensaje('No se pudo actualizar la informaci\u00F3n', 'error');
         this.onchange();
+        this.limpiarFormulario(el);
       }
     )
-    }
+
   }
 
   mostrarMensaje(mensaje: string, tipoMensaje: any) {
@@ -216,7 +241,7 @@ export class AdministracionCierreExcepcionComponent implements OnInit {
   }
   createFunForm() {
     this.funcForm = this.formBuilder.group({
-      perNom: new FormControl('', [Validators.maxLength(50), Validators.pattern(/^[a-z\s]*$/i)]),
+      perNom: new FormControl('', [Validators.maxLength(50), Validators.pattern(/^[A-Z\s]*$/)]),
       optSi: new FormControl(false),
       optNo: new FormControl(false),
       claveOperador: new FormControl(''),
@@ -228,29 +253,28 @@ export class AdministracionCierreExcepcionComponent implements OnInit {
     const listaResponse: UsuarioVO[] = [];
     this.ACEService.findAdministracionCierreExcepcion().subscribe(
       then => {
+
         for (let data of then) {
           const datos: UsuarioVO = new UsuarioVO;
           const datosCierre: CierreExcepVO = new CierreExcepVO;
           const datosSucursalVo: SucursalVO = new SucursalVO;
 
           datosCierre.idCce = data.cierreExcep.idCce
+          datosCierre.cceCierreexcep = data.cierreExcep.cceCierreexcep;
           datos.cierreExcep = datosCierre;
           datos.idPersona = data.idPersona;
           datos.perEmpleadoUsuario = data.perEmpleadoUsuario;
           datos.perNomCorto = data.perNomCorto;
           datosSucursalVo.sucSica = data.sucursalVO.sucSica;
           datos.sucursalVO = datosSucursalVo;
-
-          if (data.cierreExcep.cceCierreexcep == 'V') {
-            datos.cierreExcep.cceCierreexcep = 'Si'
-          } else {
-            datos.cierreExcep.cceCierreexcep = 'No'
-          }
           listaResponse.push(datos);
         }
+        console.log(listaResponse);
         this.dataSource = new MatTableDataSource(listaResponse);
+        this.dataSourceBkp = then;
         this.paginador = true;
         this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
         document.getElementById('paginadorDiv').removeAttribute('hidden');
       },
       error => {
@@ -258,19 +282,40 @@ export class AdministracionCierreExcepcionComponent implements OnInit {
       }
     );
   }
- deshabilitarCampos(){
-  this.funcForm.get("perNom").disable();
-  this.funcForm.get("optNo").disable();
-  this.funcForm.get("optSi").disable();
-  this.optNoDisabled = true;
-  this.optSiDisabled = true;
- }
-  ngOnInit(): void {
-
-    this.createFunForm();
-    this.deshabilitarCampos();
-    this.consulta();
-    this.atributosElemento();
+  deshabilitarCampos() {
+    this.funcForm.get("perNom").disable();
+    this.funcForm.get("optNo").disable();
+    this.funcForm.get("optSi").disable();
+    this.optNoDisabled = true;
+    this.optSiDisabled = true;
   }
 
+  sortChange(sort: Sort) {
+    console.log(sort);
+    if (!sort.active || sort.direction === '') {
+      this.dataSource.data = this.dataSourceBkp;
+      return;
+    }
+
+    this.dataSource.data = this.dataSource.data.sort((a, b) => {
+      const isAsc = sort.direction === 'asc';
+      switch (sort.active) {
+        case 'claveOperador':
+          return compare(a.perEmpleadoUsuario, b.perEmpleadoUsuario, isAsc);
+        case 'nombre':
+          return compare(a.perNomCorto, b.perNomCorto, isAsc);
+        case 'claveSucursal':
+          return compare(a.sucursalVO.sucSica, b.sucursalVO.sucSica, isAsc);
+        case 'habilitadoCierre':
+          return compare(a.cierreExcep.cceCierreexcep == 'V' ? "Si" : "No", b.cierreExcep.cceCierreexcep == 'V' ? "Si" : "No", isAsc);
+        default:
+          return 0;
+      }
+    });
+
+  }
 }
+function compare(a: number | string, b: number | string, isAsc: boolean) {
+  return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
+}
+
