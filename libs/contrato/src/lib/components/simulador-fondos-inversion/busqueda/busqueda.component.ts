38afb81d8676defb77a654233c22a994eae9bacd
Bryan Hernandez <bryan.hernandez@mtpinternational.mx>
2022-07-27 14:51:49 -0500

    Correcion de Titulo y validacion de Inputs en Simulador

--- libs/contrato/src/lib/components/simulador-fondos-inversion/busqueda/busqueda.component.ts
+++ libs/contrato/src/lib/components/simulador-fondos-inversion/busqueda/busqueda.component.ts
@@ -1,6 +1,7 @@
 import { CurrencyPipe } from '@angular/common';
-import { AfterViewInit, Component, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, EventEmitter, OnInit, Output, ViewChild, ViewChildren } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
+import { Sort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { SimuladorFondosInversionService } from '../../../services/simulador-fondos-inversion.services';
 
@@ -19,16 +20,17 @@ export class BusquedaComponent implements AfterViewInit {
 
     displayedColumns: string[] = ['fondo', 'plazo', 'ano', 'mes', 'semana', 'mes2', 'ano2'];
     displayedColumns2: string[] = ["fondo2", 'tipo', "porciento"];
-    
+
     deudaDataSource: MatTableDataSource<Object> = new MatTableDataSource();
     coberturaDataSource: MatTableDataSource<Object> = new MatTableDataSource();
     variableDataSource: MatTableDataSource<Object> = new MatTableDataSource();
 
     deudaDataSourceBkp: Object[] = [];
-    coberturaDataSourceBkp: Object[]=[];
-    variableDataSourceBkp: Object[]=[];
+    coberturaDataSourceBkp: Object[] = [];
+    variableDataSourceBkp: Object[] = [];
 
     fondoDataSource: Object[] = [];// MatTableDataSource<Object>=new MatTableDataSource([]) ;
+    fondoDataSourceBkp:Object[]=[];
     monto: string = "0";
     tipo_persona: number = 5;
     periodo: number = 1;
@@ -40,28 +42,29 @@ export class BusquedaComponent implements AfterViewInit {
     @Output() onCalcularClickEvent = new EventEmitter();
 
 
-    @ViewChild(MatPaginator) paginator: MatPaginator;
+    @ViewChildren(MatPaginator) paginadores: MatPaginator;
 
     constructor(private _simuladorFondosInversionService: SimuladorFondosInversionService, private currencyPipe: CurrencyPipe) { }
 
     ngAfterViewInit(): void {
-        this.paginator._intl.itemsPerPageLabel = "Registros por página:";
+        //this.deudaPaginator._intl.itemsPerPageLabel = "Registros por página:";
+        console.log();
     }
 
     findFondosPrecio() {
         this._simuladorFondosInversionService.findFondosPrecioVO(this.monto, this.tipo_persona, this.periodo).subscribe(then => {
 
             this.deudaDataSource = new MatTableDataSource(then.filter(item => item["tipoFondoId"] == 5));
-            this.deudaDataSourceBkp= then.filter(item => item["tipoFondoId"] == 5);
-            this.deudaDataSource.paginator = this.paginator;
+            this.deudaDataSourceBkp = then.filter(item => item["tipoFondoId"] == 5);
+            this.deudaDataSource.paginator = this.paginadores["_results"][0];
 
             this.coberturaDataSource = new MatTableDataSource(then.filter(item => item["tipoFondoId"] == 15));
             this.coberturaDataSourceBkp = then.filter(item => item["tipoFondoId"] == 15);
-            this.coberturaDataSource.paginator = this.paginator;
+            this.coberturaDataSource.paginator = this.paginadores["_results"][1];
 
             this.variableDataSource = new MatTableDataSource(then.filter(item => item["tipoFondoId"] == 10));
-            this.variableDataSourceBkp =then.filter(item => item["tipoFondoId"] == 10);
-            this.variableDataSource.paginator = this.paginator;
+            this.variableDataSourceBkp = then.filter(item => item["tipoFondoId"] == 10);
+            this.variableDataSource.paginator = this.paginadores["_results"][2];
         });
     }
 
@@ -71,28 +74,40 @@ export class BusquedaComponent implements AfterViewInit {
     }
 
     getRecord(row) {
-        this.selectedRow = row ;
+        this.selectedRow = row;
         this.fondoSelectedRow = null;
     }
 
     pasarAFondo() {
         this.fondoDataSource = [...this.fondoDataSource, this.selectedRow]//new MatTableDataSource([this.fondoDataSource.data,this.selectedRow]);
-        if(this.selectedRow["tipoFondoId"]==5){
-            this.deudaDataSourceBkp = this.deudaDataSourceBkp.filter(item => item["fondoId"] != this.selectedRow["fondoId"]);
-            this.deudaDataSource = new MatTableDataSource(this.deudaDataSourceBkp);
+        if (this.selectedRow["tipoFondoId"] == 5) {
+            this.deudaDataSource.data = this.deudaDataSource.data.filter(item => item["fondoId"] != this.selectedRow["fondoId"]);
         }
-        if(this.selectedRow["tipoFondoId"]==10){
-            this.variableDataSourceBkp = this.variableDataSourceBkp.filter(item => item["fondoId"] != this.selectedRow["fondoId"]);
-            this.variableDataSource = new MatTableDataSource(this.variableDataSourceBkp);
+        if (this.selectedRow["tipoFondoId"] == 10) {
+            this.variableDataSource.data = this.variableDataSource.data.filter(item => item["fondoId"] != this.selectedRow["fondoId"]);
+
         }
-        if(this.selectedRow["tipoFondoId"]==15){
-            this.coberturaDataSourceBkp = this.coberturaDataSourceBkp.filter(item => item["fondoId"] != this.selectedRow["fondoId"]);
-            this.coberturaDataSource = new MatTableDataSource(this.coberturaDataSourceBkp);
+        if (this.selectedRow["tipoFondoId"] == 15) {
+            this.coberturaDataSource.data = this.coberturaDataSource.data.filter(item => item["fondoId"] != this.selectedRow["fondoId"]);
         }
         this.selectedRow = null;
     }
 
     removerDeFondo() {
+
+        if (this.fondoSelectedRow["tipoFondoId"] == 5) {
+            this.deudaDataSource.data = [...this.deudaDataSource.data, this.deudaDataSourceBkp.filter(item => item["fondoId"] == this.fondoSelectedRow["fondoId"])[0]];
+        }
+
+        if (this.fondoSelectedRow["tipoFondoId"] == 10) {
+            this.variableDataSource.data = [...this.variableDataSource.data, this.variableDataSourceBkp.filter(item => item["fondoId"] == this.fondoSelectedRow["fondoId"])[0]];
+        }
+
+        if (this.fondoSelectedRow["tipoFondoId"] == 15) {
+            this.coberturaDataSource.data = [...this.coberturaDataSource.data, this.coberturaDataSourceBkp.filter(item => item["fondoId"] == this.fondoSelectedRow["fondoId"])[0]];
+        }
+
+
         this.fondoDataSource = this.fondoDataSource.filter(item => item["emisionId"] != this.fondoSelectedRow["emisionId"]);
         this.fondoSelectedRow = null;
         this.hasChenges();
@@ -116,10 +131,45 @@ export class BusquedaComponent implements AfterViewInit {
         }
     }
 
-    formatoDeNumero(e){
+    formatoDeNumero(e) {
         return this.currencyPipe.transform(e, ' ');
     }
 
+    formatoDeInput(e) {
+        console.log(e.key);
+        let reg = /[^\d]/g;
+        let texto = e.key.replace(reg, "");
+        if (texto == "" && e.key.length == 1) {
+            return false;
+        }
 
 
+        return true;
+    }
+
+
+    sortData(sort: Sort) {
+        this.fondoDataSource = this.fondoDataSource.sort((a, b) => {
+            const isAsc = sort.direction === 'asc';
+            console.log(a["emisionId"],b["emisionId"]);
+            switch (sort.active) {
+                case 'fondo2':
+                    return compare(a["emisionId"], b["emisionId"], isAsc);
+                case 'tipo':
+                    return compare(a["tipoFondoId"], b["tipoFondoId"], isAsc);
+                case 'porciento':
+
+                    return compare(a["porcentaje"], b["porcentaje"], isAsc);
+                default:
+                    return 0;
+            }
+        });
+
+    }
+
+
+}
+
+function compare(a: number | string, b: number | string, isAsc: boolean) {
+    return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
 }
